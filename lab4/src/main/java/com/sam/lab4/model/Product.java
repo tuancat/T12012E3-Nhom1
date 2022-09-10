a286defe5b2ec32c26af8085be12e937c8434a26
Ly Phi Long <philongg.ly@gmail.com>
2022-09-10 20:44:54 +0700

    tạo model product và cate

--- lab4/src/main/java/com/sam/lab4/model/Product.java
+++ lab4/src/main/java/com/sam/lab4/model/Product.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //tự động tăng
-    private int id;
+    private long id;
     private String name;
     private String description;
     @ManyToOne
--- lab4/src/main/java/com/sam/lab4/service/UserRoleService.java
+++
@@ -1,9 +0,0 @@
-package com.sam.lab4.service;
-
-import com.sam.lab4.model.UserRole;
-
-import java.util.List;
-
-public interface UserRoleService {
-    public List<UserRole> getAllUserRole();
-}
